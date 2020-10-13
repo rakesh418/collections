7277340b6ff9cc5ed04184ee621cb0c43babff42
lex418 <rakesh418@gmail.com>
2020-10-13 19:23:38 +0530

    too lazy for commit

--- src/collectionsExample.java
+++ src/collectionsExample.java
@@ -1,2 +1,10 @@
-public class collectionsExample {
+import java.util.Collections;
+import java.util.Comparator;
+
+public class CollectionsExample implements Comparator<Book> {
+    @Override
+    public int compare(CollectionsExample o1, CollectionsExample o2) {
+        Comparator.comparing()
+        return 0;
+    }
 }
